fbb700462096ca8c1984f439847d8a62b0bbef17
Abhishek-K-Vishvakarma <gitrepo101@gmail.com>
2025-06-03 11:43:38 +0530

    Adding vercel server

--- vercel_app/src/App.jsx
+++ vercel_app/src/App.jsx
@@ -3,13 +3,14 @@ import User from './User';
 function App() {
   return (
     <>
-     <h2 className="text-center text-info bg-success">Welcome to my Vercel Frontend APIs implementations</h2>
+     <h2 className="text-center text-info bg-success">Welcome to my Vercel Frontend APIs implementations
+      <Link to="/user" className="btn btn-primary">Go to User Component</Link>
+     </h2>
          <BrowserRouter>
            <Routes>
              <Route path="/user" element={<User/>}/>
            </Routes>
          </BrowserRouter>
-         <Link to="/user">User</Link>
     </>
   )
 }
