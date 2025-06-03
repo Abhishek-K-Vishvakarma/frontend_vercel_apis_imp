447217789fa3d4cd80b244ff07afca8fc2cdb804
Abhishek-K-Vishvakarma <gitrepo101@gmail.com>
2025-06-03 11:51:34 +0530

    Adding Link in Home.jsx component

--- vercel_app/src/Home.jsx
+++ vercel_app/src/Home.jsx
@@ -1,8 +1,10 @@
+import { Link } from "react-router-dom"
 
 const Home = () => {
   return (
     <div>
       <h3>Welcome! to Home Page! in frontend development!</h3>
+      <Link to="/user">Users</Link>
     </div>
   )
 }
