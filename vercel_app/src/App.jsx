d2a2cf9bc0d1e3d0f2a402a6e9cb7d7d2191c828
Abhishek-K-Vishvakarma <gitrepo101@gmail.com>
2025-06-03 11:48:01 +0530

    Adding Home.jsx component

--- vercel_app/src/App.jsx
+++ vercel_app/src/App.jsx
@@ -1,14 +1,14 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import User from './User';
+import Home from './Home';
 function App() {
   return (
     <>
-     <h2 className="text-center text-info bg-success">Welcome to my Vercel Frontend APIs implementations
-      <Link to="/user" className="btn btn-primary">Go to User Component</Link>
-     </h2>
+     <h2 className="text-center text-info bg-success">Welcome to my Vercel Frontend APIs implementations</h2>
          <BrowserRouter>
            <Routes>
              <Route path="/user" element={<User/>}/>
+             <Route path="/" element={<Home/>} />
            </Routes>
          </BrowserRouter>
     </>
